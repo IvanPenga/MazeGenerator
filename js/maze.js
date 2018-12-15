e5aec4e210ed8dd9708ae1d2a5716a94778dfceb
unknown <ivan.penga2@gmail.com>
2018-12-15 17:25:47 +0100

    Add simple UI. JS file called Code renamed to Maze. Next step is to check values for maze width and height

---
+++ js/maze.js
@@ -0,0 +1,38 @@
+function generate(){
+	
+	var canvas = document.getElementById("mycanvas");
+	var hSize = document.getElementById("hSize").value;
+	var vSize = document.getElementById("vSize").value;
+	
+	canvas.setAttribute("width",hSize);
+	canvas.setAttribute("height",vSize);
+	
+	var ctx = canvas.getContext("2d");
+
+	ctx.strokeStyle = "#FFF";
+	ctx.lineWidth = 7;
+	ctx.lineCap = "round";
+
+	var board = new Board(hSize,vSize);
+	var stack = [];
+
+	current = board.getRandomStartingPoint();
+
+	while(board.HasUnvisited){
+		neighbour = board.getRandomNeighbour(current);
+		if (neighbour != null){
+			stack.push(current);
+			board.drawLine(current, neighbour, ctx);
+			current = neighbour;
+		}
+		else if (stack.length != 0){		
+			current = stack[stack.length - 1];
+			stack.pop();
+		}	
+	}
+	
+	document.body.appendChild(canvas);
+
+}
+
+
